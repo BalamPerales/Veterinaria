89ce1fff8cbe15fbdb8c72d433e800552e533fd7
BalamPerales <balam.perales465@gmail.com>
2026-05-12 21:40:01 -0600

    feat: proyecto veterinaria con sistema de login

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Crea el usuario administrador del sistema.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@veterinaria.com'],
+            [
+                'name'     => 'admin',
+                'email'    => 'admin@veterinaria.com',
+                'password' => Hash::make('admin'),
+            ]
+        );
+    }
+}
